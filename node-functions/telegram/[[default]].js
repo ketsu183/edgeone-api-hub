d89b60ad4336defa6bc2a69fc9271f4e52dd623c
chen-zhenjie183 <1832496949@qq.com>
2026-09-04 15:03:40 +0800

    use cloud functions for telegram proxy (deploy in HK region)

--- node-functions/telegram/[[default]].js
+++ node-functions/telegram/[[default]].js
@@ -1,12 +1,9 @@
-// EdgeOne Pages Edge Function - Telegram Bot API Proxy
-// File: edge-functions/telegram/[[default]].js
+// EdgeOne Pages Cloud Function - Telegram Bot API Proxy
+// File: node-functions/telegram/[[default]].js
 // Route: /telegram/* -> https://api.telegram.org/*
 
-export async function onRequest(context) {
-  const { request } = context;
+export async function onRequest({ request }) {
   const url = new URL(request.url);
-
-  // Remove /telegram prefix and forward to api.telegram.org
   const targetPath = url.pathname.replace(/^\/telegram/, '');
   const targetUrl = 'https://api.telegram.org' + targetPath + url.search;
 
@@ -24,7 +21,7 @@ export async function onRequest(context) {
   newHeaders.set('Access-Control-Allow-Origin', '*');
   newHeaders.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, DELETE, PATCH');
   newHeaders.set('Access-Control-Allow-Headers', '*');
-  newHeaders.set('X-Proxy-By', 'EdgeOne Pages');
+  newHeaders.set('X-Proxy-By', 'EdgeOne Cloud Functions');
 
   return new Response(response.body, {
     status: response.status,
